b87f2209c8c4522f9f331a4b047fbe263907900d
Hakim Razalan <hakim.razalan@gmail.com>
2022-11-10 02:13:59 +0800

    add database provider

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,21 @@
 import "module-alias/register";
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import "reflect-metadata";
+import express, { Express } from 'express';
 import api from "@routes/api";
 import web from "@routes/web";
 
+/** Environment */
+import dotenv from 'dotenv';
 dotenv.config();
 
+/** Providers */
+import "./providers/database";
+
+/** Express */
 const app: Express = express();
 const port = process.env.PORT;
 
+/** Routes */
 app.use('/api', api);
 app.use('/', web);
 
